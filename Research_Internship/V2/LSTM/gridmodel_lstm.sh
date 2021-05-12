a0112cf90ede28461819804ed1980af6121b4841
mj0410 <66175878+mj0410@users.noreply.github.com>
2021-05-12 22:39:50 +0200

    Add files via upload

---
+++ Research_Internship/V2/LSTM/gridmodel_lstm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=lstm_gridmodel          
+#SBATCH --mail-user=miniej94@zedat.fu-berlin.de  
+#SBATCH --mail-type=end
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64G
+#SBATCH --time=1-00:00:00 
+#SBATCH --qos=standard
+
+module add Python
+module add CUDA/10.2.89-GCC-8.3.0
+module add cuDNN/7.6.4.38-gcccuda-2019b
+
+pip install tensorflow-gpu
+
+cd /scratch/miniej94
+
+python gridmodel_lstm.py
